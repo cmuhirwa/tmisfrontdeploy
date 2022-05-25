0027d25dcba016c8d5aee48427c1cc079c85a509
Mjanuary <janviermuhawenimana@gmail.com>
2022-05-26 00:29:53 +0200

    Domain updated to https

--- static/js/3.d894bca4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpTMIS=this.webpackJsonpTMIS||[]).push([[3],{112:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var e=a(0);function i(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M20,8 C18.5974037,5.04031171 15.536972,3 12,3 C7.02943725,3 3,7.02943725 3,12 C3,16.9705627 7.02943725,21 12,21 L12,21 C16.9705627,21 21,16.9705627 21,12 M21,3 L21,9 L15,9"}}]})(t)}}}]);
-//# sourceMappingURL=3.d894bca4.chunk.js.map
