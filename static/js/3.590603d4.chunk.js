b02deb3c7895fd0d7ebdd42db0bb9ec445118677
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2022-07-11 15:35:19 +0200

    Add all the changes

--- static/js/3.590603d4.chunk.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD:static/js/3.caacccd4.chunk.js
-(this.webpackJsonpTMIS=this.webpackJsonpTMIS||[]).push([[3],{118:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var e=a(0);function i(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M20,8 C18.5974037,5.04031171 15.536972,3 12,3 C7.02943725,3 3,7.02943725 3,12 C3,16.9705627 7.02943725,21 12,21 L12,21 C16.9705627,21 21,16.9705627 21,12 M21,3 L21,9 L15,9"}}]})(t)}}}]);
-//# sourceMappingURL=3.caacccd4.chunk.js.map
-=======
-(this.webpackJsonpTMIS=this.webpackJsonpTMIS||[]).push([[3],{117:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var e=a(0);function i(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M20,8 C18.5974037,5.04031171 15.536972,3 12,3 C7.02943725,3 3,7.02943725 3,12 C3,16.9705627 7.02943725,21 12,21 L12,21 C16.9705627,21 21,16.9705627 21,12 M21,3 L21,9 L15,9"}}]})(t)}}}]);
-//# sourceMappingURL=3.590603d4.chunk.js.map
->>>>>>> 0a7aa555305892b71d461279e6da26d356924de4:static/js/3.590603d4.chunk.js
