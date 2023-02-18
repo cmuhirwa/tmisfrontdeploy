b5b758d5b873cc423023293c83028ba0babae25c
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-02-18 14:29:59 +0200

    Add changes

--- static/js/64.3841c805.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpTMIS=this.webpackJsonpTMIS||[]).push([[64],{573:function(n,s,t){"use strict";t.r(s),t.d(s,"Suspension",(function(){return u}));var e=t(392),i=t(1),u=function(){return Object(i.jsx)(e.b,{thisPage:e.a.suspensionTab})}}}]);
-//# sourceMappingURL=64.3841c805.chunk.js.map
