df6d92c26aea36a571dc6c56a8374e2085502bf2
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2023-06-14 10:55:05 +0200

    Add changes

---
+++ static/js/64.2e8c49ea.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpTMIS=this.webpackJsonpTMIS||[]).push([[64],{581:function(n,s,t){"use strict";t.r(s),t.d(s,"Suspension",(function(){return u}));var e=t(392),i=t(1),u=function(){return Object(i.jsx)(e.b,{thisPage:e.a.suspensionTab})}}}]);
+//# sourceMappingURL=64.2e8c49ea.chunk.js.map
